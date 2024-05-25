4c7753b89904ba2fb480344123290b4cb82bc53c
Peter Omollo <omollpeter@gmail.com>
2024-05-25 15:22:39 +0300

    Create a handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,13 +3,18 @@
 This scripts starts flask api application
 It also registers blueprints
 """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 import os
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def error_404(error):
+    return make_response(jsonify({"error": "Not Found"}))
 
 
 @app.teardown_appcontext
