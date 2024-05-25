971b49fdf81cd58d47c7894e8531fa11cfe92c17
Peter Omollo <omollpeter@gmail.com>
2024-05-25 09:55:45 +0300

    Add get and count methods to the storage classes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,19 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Retrieves an object based on its class and id"""
+        objs = self.all(cls)
+        for key, value in objs.items():
+            if key == cls.__name__ + "." + id:
+                return value
+        return None
+
+    def count(self, cls=None):
+        """Counts the number of objects in storage"""
+        if cls:
+            count = len(self.all(cls))
+        else:
+            count = len(self.all())
+        return count
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,18 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns an object based on class and id"""
+        objs = self.all(cls)
+
+        for key, value in objs.items():
+            if key == cls.__name__ + "." + id:
+                return value
+        return None
+
+    def count(self, cls=None):
+        """Count the number of objects based on class(es)"""
+        if cls:
+            return len(self.all(cls))
+        return len(self.all())
